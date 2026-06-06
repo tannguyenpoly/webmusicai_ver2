771655a798ee5759a1c4ecca5d3cc56fc5f9b09d
Le Ngoc Hoa <ngochoa28122020@gmail.com>
2026-06-06 20:21:06 +0700

    Thêm API Admin, cập nhật tìm kiếm User và cấu hình Security test

--- src/main/java/com/fpoly/webmusicai/repository/UserRepository.java
+++ src/main/java/com/fpoly/webmusicai/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.fpoly.webmusicai.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fpoly.webmusicai.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    // Không cần viết gì thêm, JpaRepository đã bao thầu hết các lệnh CRUD cơ bản
+    // Tìm kiếm User theo Username hoặc Fullname (Có phân trang)
+    Page<User> findByUsernameContainingIgnoreCaseOrFullnameContainingIgnoreCase(String username, String fullname, Pageable pageable);
 }
