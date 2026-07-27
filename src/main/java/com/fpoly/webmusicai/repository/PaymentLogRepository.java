3ba880fe959bdc9bca564a9cba7ce1f9dc5ea4da
Le Ngoc Hoa <ngochoa28122020@gmail.com>
2026-07-27 22:23:37 +0700

    hoa/them chuc nang lich su giao dien o phan admin

--- src/main/java/com/fpoly/webmusicai/repository/PaymentLogRepository.java
+++ src/main/java/com/fpoly/webmusicai/repository/PaymentLogRepository.java
@@ -1,9 +1,18 @@
 package com.fpoly.webmusicai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.fpoly.webmusicai.entity.PaymentLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.fpoly.webmusicai.entity.PaymentLog;
 
 public interface PaymentLogRepository extends JpaRepository<PaymentLog, Integer> {
     boolean existsByTransactionId(String transactionId);
+    @Query("SELECT p FROM PaymentLog p WHERE " +
+           "(:keyword IS NULL OR p.orderCode LIKE %:keyword% OR p.transactionId LIKE %:keyword% OR p.content LIKE %:keyword%)")
+    Page<PaymentLog> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
+
 }
